1de7bf4183e13bc9c92c094bbf2cfba1e7f50682
PrashantRaghuwanshi07 <prashant.raghuwanshi2006@gmail.com>
2026-05-16 15:38:15 +0530

    Prime No and Armstrong No

---
+++ src/Functions/Shadowing.java
@@ -0,0 +1,17 @@
+package Functions;
+
+public class Shadowing {
+    static int x = 10;
+
+    static void main(String[] args) {
+        System.out.println(x);
+        int x; // class variable at line 4 is shadowed by this
+//        System.out.println(x);//scope will begin when value is initialised
+        x = 40;
+        System.out.println(x); //40
+        fun();
+    }
+    static void fun() {
+        System.out.println(x);
+    }
+}
